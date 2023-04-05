3cf49f9f53575cbcf8d4c5105ffb9a7614c60d98
joao123kiol <129911706+joao123kiol@users.noreply.github.com>
2023-04-04 21:07:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function bodyLoad()
+{
+  document.getElementById("bt").style.fontSize = "15px";
+  document.getElementById("bt").style.backgroundColor="darkred";
+  document.getElementById("bt").style.BorderRadius="20px";
+  document.getElementById("bt").style.color="withe"
+  document.getElementById("bt").style.padding="10px"
+  document.getElementById("bt").style.borderStyle="none"
+}
+function changetheme ()
+{
+    document.getElementById("body").style.backgroundColor="darkblue"
+;document.getElementById("nometitulo").style.backgroundColor="darkblue"
+ document.getElementById("anchorTag").style.color="darkblue"
+
+ document.getElementById("anchorTag").style.borderWidth}
